ef0d9a39bdafaaa731f5e78391e0d4753c4c1cf8
gaord <2068586788@qq.com>
2015-10-23 15:53:33 +0800

    tabestry+tomcat

---
+++ mt/src/main/java/grd/pages/Start.java
@@ -0,0 +1,16 @@
+package grd.pages;
+
+public class Start {
+	
+	private String name;
+
+	public String getName() {
+		return "mmm!";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
